4b4128488bacd96ac8d9456b06bfcc5172cda508
tkumar07 <72886906+tkumar07@users.noreply.github.com>
2022-10-26 01:03:50 +0000

    teditedRepository ID field

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
@@ -9,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, String> {
-    Iterable<UCSBOrganization> findOrganization(String organization);
+    Iterable<UCSBOrganization> findOrganization(String orgCode);
 }
