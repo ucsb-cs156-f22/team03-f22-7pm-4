acdd65938d84e69bdc8066e9acb8578196ddcd26
tkumar07 <72886906+tkumar07@users.noreply.github.com>
2022-11-10 22:23:28 +0000

    tk - full functionality of list organizations

--- frontend/src/main/pages/Organizations/OrganizationsIndexPage.js
+++ frontend/src/main/pages/Organizations/OrganizationsIndexPage.js
@@ -1,13 +1,28 @@
-import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import React from 'react'
+import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
 
-export default function OrganizationsIndexPage() {
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import UCSBOrganizationsTable from 'main/components/Organizations/UCSBOrganizationsTable';
+import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
+
+export default function UCSBOrganizationsIndexPage() {
+
+  const currentUser = useCurrentUser();
+
+  const { data: organizations, error: _error, status: _status } =
+    useBackend(
+      // Stryker disable next-line all : don't test internal caching of React Query
+      ["/api/ucsborganization/all"],
+            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
+            { method: "GET", url: "/api/ucsborganization/all" },
+      []
+    );
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Organizations</h1>
-        <p>
-          This is where the index page will go
-        </p>
+        <h1>UCSB Organizations</h1>
+        <UCSBOrganizationsTable organizations={organizations} currentUser={currentUser} />
       </div>
     </BasicLayout>
   )
