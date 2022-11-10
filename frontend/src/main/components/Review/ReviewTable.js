6b0fcf405e357750d0dddde89a3e182314a2b0b7
ruiqiRichard <ruiqiyang.richard@gmail.com>
2022-11-09 23:11:10 -0800

    RY-add delete column for ReviewTable

--- frontend/src/main/components/Review/ReviewTable.js
+++ frontend/src/main/components/Review/ReviewTable.js
@@ -1,39 +1,43 @@
 import OurTable, { ButtonColumn} from "main/components/OurTable";
 import { useBackendMutation } from "main/utils/useBackend";
 import {  onDeleteSuccess } from "main/utils/UCSBDateUtils"
-import { useNavigate } from "react-router-dom";
+import { _useNavigate } from "react-router-dom";
 import { hasRole } from "main/utils/currentUser";
 
-// export function cellToAxiosParamsDelete(cell) {
-//     return {
-//         url: "/api/menuitemreview",
-//         method: "DELETE",
-//         params: {
-//             code: cell.row.values.code
-//         }
-//     }
-// }
+export function cellToAxiosParamsDelete(cell) {
+    return {
+        url: "/api/menuitemreview",
+        method: "DELETE",
+        params: {
+            id: cell.row.values.id
+        }
+    }
+}
 
 export default function ReviewTable({ review, currentUser }) {
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     // const editCallback = (cell) => {
-    //     navigate(`/review/edit/${cell.row.values.code}`)
+    //     navigate(`/review/edit/${cell.row.values.id}`)
     // }
 
-    // // Stryker disable all : hard to test for query caching
-    // const deleteMutation = useBackendMutation(
-    //     cellToAxiosParamsDelete,
-    //     { onSuccess: onDeleteSuccess },
-    //     ["/api/menuitemreview/all"]
-    // );
-    // // Stryker enable all 
+    // Stryker disable all : hard to test for query caching
+    const deleteMutation = useBackendMutation(
+        cellToAxiosParamsDelete,
+        { onSuccess: onDeleteSuccess },
+        ["/api/menuitemreview/all"]
+    );
+    // Stryker enable all 
 
-    // // Stryker disable next-line all : TODO try to make a good test for this
-    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+    // Stryker disable next-line all : TODO try to make a good test for this
+    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
 
     const columns = [
+        {
+            Header: 'id',
+            accessor: 'id', // accessor is the "key" in the data
+        },
         {
             Header: 'Item ID',
             accessor: 'itemId', 
@@ -59,14 +63,13 @@ export default function ReviewTable({ review, currentUser }) {
 
     const testid = "ReviewTable";
 
-    // const columnsIfAdmin = [
-    //     ...columns,
-    //     ButtonColumn("Edit", "primary", editCallback, testid),
-    //     ButtonColumn("Delete", "danger", deleteCallback, testid)
-    // ];
+    const columnsIfAdmin = [
+        ...columns,
+        // ButtonColumn("Edit", "primary", editCallback, testid),
+        ButtonColumn("Delete", "danger", deleteCallback, testid)
+    ];
 
-    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
-    const columnsToDisplay = columns;
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
 
     return <OurTable
         data={review}
