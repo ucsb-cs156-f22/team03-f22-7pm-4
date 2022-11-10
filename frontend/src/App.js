40b0956ea5ec90e2f9bc856b13914cdcda824289
ruiqiRichard <ruiqiyang.richard@gmail.com>
2022-11-09 17:47:34 -0800

    RY-Create review index page and link it to the nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ import UCSBDatesIndexPage from "main/pages/UCSBDates/UCSBDatesIndexPage";
 import UCSBDatesCreatePage from "main/pages/UCSBDates/UCSBDatesCreatePage";
 import UCSBDatesEditPage from "main/pages/UCSBDates/UCSBDatesEditPage";
 
+import ReviewIndexPage from "main/pages/Review/ReviewIndexPage";
+
 
 import { hasRole, useCurrentUser } from "main/utils/currentUser";
 
@@ -70,6 +72,13 @@ function App() {
             <>
               <Route exact path="/ucsbdates/create" element={<UCSBDatesCreatePage />} />
               <Route exact path="/ucsbdates/edit/:id" element={<UCSBDatesEditPage />} />
+            </>
+          )
+        }
+        {
+          hasRole(currentUser, "ROLE_USER") && (
+            <>
+              <Route exact path="/review/list" element={<ReviewIndexPage />} />
             </>
           )
         }
