6c18916543e8cb9b39ae33c60b0252ea82c9b5e0
Archit Gupta <archit@ucsb.edu>
2022-11-12 19:56:55 -0800

    Revert "added delete column"
    This reverts commit 63c1027ff19ce9cc8a6f368753d9ca224c9b54a8.

--- frontend/src/main/components/HelpRequests/HelpRequestsTable.js
+++ frontend/src/main/components/HelpRequests/HelpRequestsTable.js
@@ -1,27 +1,11 @@
-import OurTable, { ButtonColumn } from "main/components/OurTable";
-import { useBackendMutation } from "main/utils/useBackend";
-import { onDeleteSuccess } from "main/utils/UCSBDateUtils"
-import { hasRole } from "main/utils/currentUser";
-import { cellToAxiosParamsDelete } from "main/utils/HelpRequestsUtils";
+import OurTable from "main/components/OurTable";
 
-
-export default function HelpRequestsTable({ helpRequests, currentUser }) {
-
-    // Stryker disable all : hard to test for query caching
-    const deleteMutation = useBackendMutation(
-        cellToAxiosParamsDelete,
-        { onSuccess: onDeleteSuccess },
-        ["/api/HelpRequest/all"]
-    );
-    // Stryker enable all 
-
-    // Stryker disable next-line all : TODO try to make a good test for this
-    const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+export default function HelpRequestsTable({ helpRequests, _currentUser }) {
 
     const columns = [
         {
             Header: 'ID',
-            accessor: 'id',
+            accessor: 'id', 
         },
         {
             Header: 'Requester Email',
@@ -45,22 +29,14 @@ export default function HelpRequestsTable({ helpRequests, currentUser }) {
         },
         {
             Header: 'Solved?',
-            id: 'solved',
-            accessor: (row, _rowIndex) => String(row.solved)
+            id: 'solved', 
+            accessor: (row, _rowIndex) => String(row.solved) 
         }
     ];
 
-    const columnsIfAdmin = [
-        ...columns,
-        ButtonColumn("Delete", "danger", deleteCallback, "HelpRequestsTable")
-    ];
-
-    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
-
-
     return <OurTable
         data={helpRequests}
-        columns={columnsToDisplay}
+        columns={columns}
         testid={"HelpRequestsTable"}
     />;
 };
