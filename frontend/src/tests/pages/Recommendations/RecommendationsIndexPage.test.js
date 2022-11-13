401f5fa8f780bf2e0a137f96a9ec2228ce1b6cba
FinalGale <46613691+FinalGale@users.noreply.github.com>
2022-11-12 21:52:14 -0800

    100% code coverage now

--- frontend/src/tests/pages/Recommendations/RecommendationsIndexPage.test.js
+++ frontend/src/tests/pages/Recommendations/RecommendationsIndexPage.test.js
@@ -143,4 +143,34 @@ describe("RecommendationIndexPage tests", () => {
     
         expect(queryByTestId(`${testId}-cell-row-0-col-id`)).not.toBeInTheDocument();
     });
+
+    test("test what happens when you click delete, admin", async () => {
+        setupAdminUser();
+
+        const queryClient = new QueryClient();
+        axiosMock.onGet("/api/Recommendation/all").reply(200, recommendationsFixtures.threeRecommendations);
+        axiosMock.onDelete("/api/Recommendation").reply(200, "Recommendation with id 0 was deleted");
+
+
+        const { getByTestId } = render(
+            <QueryClientProvider client={queryClient}>
+                <MemoryRouter>
+                    <RecommendationsIndexPage />
+                </MemoryRouter>
+            </QueryClientProvider>
+        );
+
+        await waitFor(() => { expect(getByTestId(`${testId}-cell-row-0-col-id`)).toBeInTheDocument(); });
+
+        expect(getByTestId(`${testId}-cell-row-0-col-id`)).toHaveTextContent("0"); 
+
+
+        const deleteButton = getByTestId(`${testId}-cell-row-0-col-Delete-button`);
+        expect(deleteButton).toBeInTheDocument();
+       
+        fireEvent.click(deleteButton);
+
+        await waitFor(() => { expect(mockToast).toBeCalledWith("Recommendation with id 0 was deleted") });
+
+    });
 });
