39bc46dcf830e55b08b4e54c47805b7154deb55f
nathan <nathangeorgemathew30@gmail.com>
2022-11-14 17:36:31 -0800

    Adds article table to storybook

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,39 @@
+const articlesFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "title1",
+        "url": "a.com",
+        "explanation": "ex1",
+        "email": "a@gmail.com",
+        "dateAdded": "2020-10-10T11:11:11"
+      },
+    threeArticles: [
+        {
+            "id": 1,
+            "title": "title1",
+            "url": "a.com",
+            "explanation": "ex1",
+            "email": "a@gmail.com",
+            "dateAdded": "2020-10-10T11:11:11"
+          },
+          {
+            "id": 2,
+            "title": "title2",
+            "url": "b.com",
+            "explanation": "ex2",
+            "email": "b@gmail.com",
+            "dateAdded": "2020-10-10T12:12:12"
+          },
+          {
+            "id": 3,
+            "title": "title3",
+            "url": "c.com",
+            "explanation": "ex3",
+            "email": "c@gmail.com",
+            "dateAdded": "2020-10-10T10:10:10"
+          }
+    ]
+};
+
+
+export { articlesFixtures };
---
+++ frontend/src/main/components/Articles/ArticlesTable.js
@@ -0,0 +1,79 @@
+import OurTable from "main/components/OurTable";
+// import { useBackendMutation } from "main/utils/useBackend";
+// import { cellToAxiosParamsDelete, onDeleteSuccess } from "main/utils/UCSBDateUtils"
+// import { useNavigate } from "react-router-dom";
+// import { hasRole } from "main/utils/currentUser";
+
+export default function ArticlesTable({ articles, _currentUser }) {
+
+    // const navigate = useNavigate();
+
+    // const editCallback = (cell) => {
+    //     navigate(`/ucsbdates/edit/${cell.row.values.id}`)
+    // }
+
+    // Stryker disable all : hard to test for query caching
+    // const deleteMutation = useBackendMutation(
+    //     cellToAxiosParamsDelete,
+    //     { onSuccess: onDeleteSuccess },
+    //     ["/api/ucsbdates/all"]
+    // );
+    // Stryker enable all 
+
+    // Stryker disable next-line all : TODO try to make a good test for this
+    // const deleteCallback = async (cell) => { deleteMutation.mutate(cell); }
+
+
+    // {
+    //     "id": 3,
+    //     "title": "title",
+    //     "url": "a.com",
+    //     "explanation": "ex",
+    //     "email": "a@gmail.com",
+    //     "dateAdded": "2020-10-10T11:11:11"
+    //   }
+
+
+    const columns = [
+        {
+            Header: 'id',
+            accessor: 'id', // accessor is the "key" in the data
+        },
+        {
+            Header: 'Title',
+            accessor: 'title',
+        },
+        {
+            Header: 'Url',
+            accessor: 'url',
+        },
+        {
+            Header: 'Explanation',
+            accessor: 'explanation',
+        },
+        {
+            Header: 'Email',
+            accessor: 'email',
+        },
+        {
+            Header: 'Date Added',
+            accessor: 'dateAdded',
+        }
+    ];
+
+    // const columnsIfAdmin = [
+    //     ...columns,
+    //     ButtonColumn("Edit", "primary", editCallback, "UCSBDatesTable"),
+    //     ButtonColumn("Delete", "danger", deleteCallback, "UCSBDatesTable")
+    // ];
+
+    // const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    const columnsToDisplay = columns;
+
+    return <OurTable
+        data={articles}
+        columns={columnsToDisplay}
+        testid={"ArticlesTable"}
+    />;
+};
